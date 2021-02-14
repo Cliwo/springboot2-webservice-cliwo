aaf440bd0e9c879ffe03dc294dfd55249ee22c73
cliwo <cliwo@naver.com>
2021-02-14 16:12:57 +0900

    Chapter 04 - ~151p
    - Posts 리스트 보는것 구현
    - Post 등록 구현
    - 수정, 삭제 기능 구현 이전

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing //이거 있으면 기존 test 실패함. 'PostsRepositoryTest - BaseTimeEntity_등록' 에서만 사용
+@EnableJpaAuditing //이거 있으면 기존 test 실패함. 'PostsRepositoryTest - BaseTimeEntity_등록' 에서만 사용
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     //JpaRepository를 상속하면 기본 CRUD 메소드가 자동 생성된다.
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
