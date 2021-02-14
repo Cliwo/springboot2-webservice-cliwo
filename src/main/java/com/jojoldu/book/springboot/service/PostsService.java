170b22f21eead3c5321e4e2207d109b349b25279
cliwo <cliwo@naver.com>
2021-02-14 16:32:26 +0900

    Chapter 04 - ~161p (완)
    - 수정, 삭제 기능 구현

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -37,6 +37,14 @@ public class PostsService {
     }
 
     @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional
     public PostsResponseDto findById(Long id)
     {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
